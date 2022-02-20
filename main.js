470c892b92b845ede79644b8c42c25c3483d5cf1
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-02-20 10:47:51 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+﻿function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(500, 400);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function takeSnapshot() {
+    save("selfie.png");
+}
